20313adf3af43af74efe999aa03c3a234aa76abd
andreibaidoc <60269675+andreibaidoc@users.noreply.github.com>
2025-05-08 12:07:24 +0200

    Added Bluetooth interface and COM choice for the user.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,23 +34,37 @@
             this.Title = new System.Windows.Forms.Label();
             this.SubtitleDescriptionLabel = new System.Windows.Forms.Label();
             this.connections_label = new System.Windows.Forms.Label();
+            this.status_label = new System.Windows.Forms.Label();
+            this.connect_bluetooth_button = new System.Windows.Forms.Button();
+            this.bluetooth_status_label = new System.Windows.Forms.Label();
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBoxUsbPorts = new System.Windows.Forms.ComboBox();
+            this.comboBoxBTPorts = new System.Windows.Forms.ComboBox();
+            this.refresh_com_list_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
             // 
             // connect_usb_button
             // 
             this.connect_usb_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.connect_usb_button.Location = new System.Drawing.Point(61, 256);
+            this.connect_usb_button.Location = new System.Drawing.Point(56, 246);
+            this.connect_usb_button.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.connect_usb_button.Name = "connect_usb_button";
-            this.connect_usb_button.Size = new System.Drawing.Size(192, 44);
+            this.connect_usb_button.Size = new System.Drawing.Size(242, 40);
             this.connect_usb_button.TabIndex = 0;
             this.connect_usb_button.Text = "Connect via USB";
             this.connect_usb_button.UseVisualStyleBackColor = true;
+            this.connect_usb_button.Click += new System.EventHandler(this.connect_usb_button_Click);
             // 
             // Title
             // 
             this.Title.AutoSize = true;
             this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Title.Location = new System.Drawing.Point(38, 48);
+            this.Title.Location = new System.Drawing.Point(35, 44);
+            this.Title.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Title.Name = "Title";
             this.Title.Size = new System.Drawing.Size(364, 32);
             this.Title.TabIndex = 1;
@@ -59,9 +73,10 @@
             // SubtitleDescriptionLabel
             // 
             this.SubtitleDescriptionLabel.AutoSize = true;
-            this.SubtitleDescriptionLabel.Location = new System.Drawing.Point(39, 93);
+            this.SubtitleDescriptionLabel.Location = new System.Drawing.Point(35, 85);
+            this.SubtitleDescriptionLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.SubtitleDescriptionLabel.Name = "SubtitleDescriptionLabel";
-            this.SubtitleDescriptionLabel.Size = new System.Drawing.Size(576, 75);
+            this.SubtitleDescriptionLabel.Size = new System.Drawing.Size(584, 69);
             this.SubtitleDescriptionLabel.TabIndex = 2;
             this.SubtitleDescriptionLabel.Text = "This program lets the user retrieve the data from the ExploRover \r\nthrough difere" +
     "nt methods, such that the user can import, visualize \r\nand examine the humidity," +
@@ -70,27 +85,93 @@
             // connections_label
             // 
             this.connections_label.AutoSize = true;
-            this.connections_label.Location = new System.Drawing.Point(39, 211);
+            this.connections_label.Location = new System.Drawing.Point(35, 195);
+            this.connections_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.connections_label.Name = "connections_label";
-            this.connections_label.Size = new System.Drawing.Size(186, 25);
+            this.connections_label.Size = new System.Drawing.Size(182, 23);
             this.connections_label.TabIndex = 3;
             this.connections_label.Text = "Connection options:";
             // 
+            // status_label
+            // 
+            this.status_label.AutoSize = true;
+            this.status_label.Location = new System.Drawing.Point(327, 255);
+            this.status_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.status_label.Name = "status_label";
+            this.status_label.Size = new System.Drawing.Size(72, 23);
+            this.status_label.TabIndex = 4;
+            this.status_label.Text = "Status:";
+            // 
+            // connect_bluetooth_button
+            // 
+            this.connect_bluetooth_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connect_bluetooth_button.Location = new System.Drawing.Point(56, 350);
+            this.connect_bluetooth_button.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.connect_bluetooth_button.Name = "connect_bluetooth_button";
+            this.connect_bluetooth_button.Size = new System.Drawing.Size(242, 40);
+            this.connect_bluetooth_button.TabIndex = 5;
+            this.connect_bluetooth_button.Text = "Connect via Bluetooth";
+            this.connect_bluetooth_button.UseVisualStyleBackColor = true;
+            this.connect_bluetooth_button.Click += new System.EventHandler(this.connect_bluetooth_button_Click);
+            // 
+            // bluetooth_status_label
+            // 
+            this.bluetooth_status_label.AutoSize = true;
+            this.bluetooth_status_label.Location = new System.Drawing.Point(327, 359);
+            this.bluetooth_status_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.bluetooth_status_label.Name = "bluetooth_status_label";
+            this.bluetooth_status_label.Size = new System.Drawing.Size(72, 23);
+            this.bluetooth_status_label.TabIndex = 6;
+            this.bluetooth_status_label.Text = "Status:";
+            // 
+            // comboBoxUsbPorts
+            // 
+            this.comboBoxUsbPorts.FormattingEnabled = true;
+            this.comboBoxUsbPorts.Location = new System.Drawing.Point(56, 292);
+            this.comboBoxUsbPorts.Name = "comboBoxUsbPorts";
+            this.comboBoxUsbPorts.Size = new System.Drawing.Size(242, 31);
+            this.comboBoxUsbPorts.TabIndex = 7;
+            // 
+            // comboBoxBTPorts
+            // 
+            this.comboBoxBTPorts.FormattingEnabled = true;
+            this.comboBoxBTPorts.Location = new System.Drawing.Point(56, 397);
+            this.comboBoxBTPorts.Name = "comboBoxBTPorts";
+            this.comboBoxBTPorts.Size = new System.Drawing.Size(242, 31);
+            this.comboBoxBTPorts.TabIndex = 8;
+            // 
+            // refresh_com_list_button
+            // 
+            this.refresh_com_list_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refresh_com_list_button.Location = new System.Drawing.Point(56, 470);
+            this.refresh_com_list_button.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.refresh_com_list_button.Name = "refresh_com_list_button";
+            this.refresh_com_list_button.Size = new System.Drawing.Size(242, 40);
+            this.refresh_com_list_button.TabIndex = 9;
+            this.refresh_com_list_button.Text = "Refresh COM List";
+            this.refresh_com_list_button.UseVisualStyleBackColor = true;
+            this.refresh_com_list_button.Click += new System.EventHandler(this.refresh_com_list_button_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1244, 776);
+            this.ClientSize = new System.Drawing.Size(1178, 744);
+            this.Controls.Add(this.refresh_com_list_button);
+            this.Controls.Add(this.comboBoxBTPorts);
+            this.Controls.Add(this.comboBoxUsbPorts);
+            this.Controls.Add(this.bluetooth_status_label);
+            this.Controls.Add(this.connect_bluetooth_button);
+            this.Controls.Add(this.status_label);
             this.Controls.Add(this.connections_label);
             this.Controls.Add(this.SubtitleDescriptionLabel);
             this.Controls.Add(this.Title);
             this.Controls.Add(this.connect_usb_button);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Form1";
+            this.ShowIcon = false;
             this.Text = "Explorover Data Retriever";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,6 +184,13 @@
         private System.Windows.Forms.Label Title;
         private System.Windows.Forms.Label SubtitleDescriptionLabel;
         private System.Windows.Forms.Label connections_label;
+        private System.Windows.Forms.Label status_label;
+        private System.Windows.Forms.Button connect_bluetooth_button;
+        private System.Windows.Forms.Label bluetooth_status_label;
+        private System.IO.Ports.SerialPort serialPort2;
+        private System.Windows.Forms.ComboBox comboBoxUsbPorts;
+        private System.Windows.Forms.ComboBox comboBoxBTPorts;
+        private System.Windows.Forms.Button refresh_com_list_button;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,25 +1,91 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Ports;
 using System.Windows.Forms;
 
 namespace Arduino_GUI_for_Data_Retrieval
 {
     public partial class Form1 : Form
     {
+        // Variables
+        private bool connected_usb = false;
+        private bool connected_bluetooth = false;
+
         public Form1()
         {
             InitializeComponent();
+            InitializeParameters();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void InitializeParameters()
         {
+            status_label.Visible = false; // make the status label invisible at the start - status label for USB connection
+            bluetooth_status_label.Visible = false; // make the bluetooth status label invisible at the start - status label for Bluetooth connection
+        }
 
+        private void PopulatePortLists()
+        {
+            // get ALL COM ports
+            var ports = SerialPort.GetPortNames();
+
+            // USB list
+            comboBoxUsbPorts.Items.Clear();
+            comboBoxUsbPorts.Items.AddRange(ports);
+            if (comboBoxUsbPorts.Items.Count > 0)
+                comboBoxUsbPorts.SelectedIndex = 0;
+
+            // BT list (same ports – after you pair your HC-05 it shows up here too)
+            comboBoxBTPorts.Items.Clear();
+            comboBoxBTPorts.Items.AddRange(ports);
+            if (comboBoxBTPorts.Items.Count > 0)
+                comboBoxBTPorts.SelectedIndex = 0;
+        }
+
+        private void connect_usb_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBoxUsbPorts.Text;
+                serialPort1.BaudRate = 9600;
+                serialPort1.Open();
+
+                status_label.Text = "Status: Connected!";
+                status_label.ForeColor = Color.Green;
+                status_label.Visible = true;
+                connected_usb = true;
+            }
+            catch (Exception ex)
+            {
+                status_label.Text = $"Could not open USB port: {ex.Message}";
+                status_label.ForeColor = Color.Red;
+                status_label.Visible = true;
+            }
+        }
+
+        private void connect_bluetooth_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort2.PortName = comboBoxBTPorts.Text;
+                serialPort2.BaudRate = 9600;
+                serialPort2.Open();
+
+                bluetooth_status_label.Text = "BT: Connected!";
+                bluetooth_status_label.ForeColor = Color.Green;
+                bluetooth_status_label.Visible = true;
+                connected_bluetooth = true;
+            }
+            catch (Exception ex)
+            {
+                bluetooth_status_label.Text = $"Could not open BT port: {ex.Message}";
+                bluetooth_status_label.ForeColor = Color.Red;
+                bluetooth_status_label.Visible = true;
+            }
+        }
+
+        private void refresh_com_list_button_Click(object sender, EventArgs e)
+        {
+            PopulatePortLists();
         }
     }
 }
